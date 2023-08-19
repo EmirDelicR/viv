7a8e88963db956f0b353b158e40e059649ac4bf6
EmirDelicR <emirdelictbf@gmail.com>
2023-08-19 10:29:33 +0200

    Added formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,56 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    "plugin:react/recommended",
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+     "plugin:react/jsx-runtime",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "eslint-config-prettier"  
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: "detect"
+    },
+    "import/resolver": {
+      node: {
+        "paths": [
+          "src"
+        ],
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx"
+        ]
+      }
+    }
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "no-unused-vars": [
+      "error",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_"
+      }
+    ],
+    "react/react-in-jsx-scope": "off",
+    "import/extensions": 0,
+    "import/no-unresolved": 0,
+    "no-console": 0,
+    "object-curly-newline": 0,
+    "import/prefer-default-export": 0,
+    "comma-dangle": ["error", "only-multiline"],
+    "@typescript-eslint/no-non-null-assertion": 0
   },
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
-
+ 
 import App from './UI/App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
