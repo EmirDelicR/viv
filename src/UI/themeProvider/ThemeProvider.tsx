a6f65a4c906cf641a8b43b8041a416ab68253e87
EmirDelicR <emirdelictbf@gmail.com>
2023-09-21 12:44:01 +0200

    Add variables

--- src/UI/themeProvider/ThemeProvider.tsx
+++ src/UI/themeProvider/ThemeProvider.tsx
@@ -1,5 +1,7 @@
 import {
+  CSSVariablesResolver,
   MantineProvider,
+  MantineTheme,
   createTheme,
   localStorageColorSchemeManager
 } from '@mantine/core';
@@ -12,11 +14,27 @@ const theme = createTheme({
   fontFamily: 'Open Sans, sans-serif'
 });
 
+const resolver: CSSVariablesResolver = (theme: MantineTheme) => ({
+  variables: {
+    '--my-primary-color': theme.colors.blue[5]
+  },
+  light: {
+    '--my-primary-color': theme.colors.blue[5]
+  },
+  dark: {
+    '--my-primary-color': theme.colors.blue[5]
+  }
+});
+
 export default function ThemeProvider({ children }: PropsWithChildren) {
   const colorSchemeManager = localStorageColorSchemeManager({ key: THEME_KEY });
 
   return (
-    <MantineProvider colorSchemeManager={colorSchemeManager} theme={theme}>
+    <MantineProvider
+      colorSchemeManager={colorSchemeManager}
+      theme={theme}
+      cssVariablesResolver={resolver}
+    >
       {children}
     </MantineProvider>
   );
